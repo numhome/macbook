901e392472082b38069648c0623f1701be90503f
numhome <79175337+numhome@users.noreply.github.com>
2022-02-07 01:11:21 +0100

    Update veru.sh

--- veru.sh
+++ veru.sh
@@ -1,5 +1,5 @@
 # veruscoin
 sudo apt update
 wget https://github.com/hellcatz/luckpool/raw/master/miners/hellminer_cpu_linux.tar.gz
-tar -xaf hellminer_cpu_linux.tar.gz
+tar -xf hellminer_cpu_linux.tar.gz
 ./hellminer -c stratum+tcp://eu.luckpool.net:3956 -u RC8YDpAxoT5D66B2uUaFyNFYgbAgn6QjGn.Farm1-p x --cpu 4
